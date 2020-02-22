fbca8aff7b158bf1d4097c422b92fc51e5210fd1
Wiratamana <wiratamana@icloud.com>
2020-02-23 07:40:18 +0900

    dummy ai will play hitted animation when player attacked

---
+++ RPG/Assets/Scripts/Status/Status_DamageData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tamana
+{
+    public struct Status_DamageData
+    {
+        public int damagePoint;
+        public bool isIgnoreDefense;
+    }
+}
