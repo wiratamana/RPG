ccfd2c748adde9b210a2384a8d62b41679702256
Wiratamana <wiratamana@icloud.com>
2020-03-06 07:06:52 +0900

    improve monobehaviour extension utility

--- RPG/Assets/Scripts/Utils/MonoBehaviourExtension.cs
+++ RPG/Assets/Scripts/Utils/MonoBehaviourExtension.cs
@@ -30,12 +30,66 @@ namespace Tamana
             return value;
         }
 
+        public static T GetAndAssignComponentInChildren<T>(this MonoBehaviour component, ref T value) where T : Component
+        {
+            if (value == null)
+            {
+                value = component.GetComponentInChildren<T>();
+            }
+
+            return value;
+        }
+
+        public static T GetAndAssignComponentInParent<T>(this MonoBehaviour component, ref T value) where T : Component
+        {
+            if (value == null)
+            {
+                value = component.GetComponentInParent<T>();
+            }
+
+            return value;
+        }
+
+        public static Transform GetChildWithNameAndAssign(this MonoBehaviour component, string childName, ref Transform value)
+        {
+            if(value == null)
+            {
+                value = GetChildWithNameRecursive(childName, component.transform);
+            }
+
+            return value;
+        }
+
         public static void LogErrorIfComponentIsNull<T>(this MonoBehaviour component, T value) where T : Component
         {
             if(value == null)
             {
                 Debug.Log($"Component '{typeof(T).Name}' is not exist on GameObject with name '{component.name}'.", Debug.LogType.Error);
             }
+        }
+
+        private static Transform GetChildWithNameRecursive(string childName, Transform parent)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (child.name != childName)
+                {
+                    child = GetChildWithNameRecursive(childName, child);
+                }
+
+                if (child == null)
+                {
+                    continue;
+                }
+
+                if (child.name == childName)
+                {
+                    return child;
+                }
+            }
+
+            return null;
         }
     }
 }
