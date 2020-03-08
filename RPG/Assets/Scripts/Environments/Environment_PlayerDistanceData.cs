a9235ae8a56c0d8471191988c621e84c8a2a8885
Wiratamana <wiratamana@icloud.com>
2020-03-09 08:32:05 +0900

    automatically add and destroy meshcollider base on distance from object position to player position

---
+++ RPG/Assets/Scripts/Environments/Environment_PlayerDistanceData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Tamana
+{
+    public struct Environment_PlayerDistanceData
+    {
+        private float distanceToPlayer;
+        private readonly Vector3 myPos;
+        private readonly Vector3 playerPos;
+        private readonly int objectInstanceID;
+
+        public Environment_PlayerDistanceData(Vector3 myPos, Vector3 playerPos, int objectInstanceID)
+        {
+            this.myPos = myPos;
+            this.playerPos = playerPos;
+            this.objectInstanceID = objectInstanceID;
+            distanceToPlayer = 0.0f;
+        }
+
+        public void Calculate()
+        {
+            distanceToPlayer = Vector3.Distance(myPos, playerPos);
+ 
+            if (distanceToPlayer < 10)
+            {
+                Environment_Main.idWithDistanceBelow10.Enqueue(objectInstanceID);
+            }
+            else if(distanceToPlayer >= 10 && distanceToPlayer < 15)
+            {
+                Environment_Main.idWithDistanceBetween10and15.Enqueue(objectInstanceID);
+            }
+        }
+    }
+}
