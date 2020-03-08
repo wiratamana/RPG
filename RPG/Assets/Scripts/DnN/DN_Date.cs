c4e3bbcd47e520c9860ac2b54af7cdac3dd734d2
Wiratamana <wiratamana@icloud.com>
2020-03-08 20:16:54 +0900

    implement basic day and night cycle

---
+++ RPG/Assets/Scripts/DnN/DN_Date.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct DN_Date
+{
+    public int Day { get; }
+    public int Hour { get; }
+    public int Minute { get; }
+
+    public DN_Date(int day, int hour, int minute)
+    {
+        Day = day;
+        Hour = hour;
+        Minute = minute;
+    }
+
+    public override string ToString()
+    {
+        return $"Day {Day} - {Hour.ToString("00")}:{Minute.ToString("00")}";
+    }
+}
---
+++ RPG/Assets/Scripts/DnN/DN_LightingManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Tamana
+{
+    public class DN_LightingManager : MonoBehaviour
+    {
+        [SerializeField] private DN_LightingPreset LightingPreset;
+        [SerializeField] private TextMeshProUGUI dateUI;
+        private Light directionalLight;
+        private DN_Main main;
+        public DN_Main Main => this.GetAndAssignComponent(ref main);
+        private int dateDay = 0;
+        private float TimeOfDay = 8;
+
+        private void OnValidate()
+        {
+            directionalLight = GetComponentInChildren<Light>();
+        }
+
+        private void Update()
+        {
+            TimeOfDay += Time.deltaTime / 60.0f;
+            var day = TimeOfDay % 24;
+            if (day < TimeOfDay)
+            {
+                dateDay++;
+            }
+            var dateHour = (int)day;
+            var dateMinute = (day - dateHour) * 60.0f;
+            dateUI.text = new DN_Date(dateDay, dateHour, (int)dateMinute).ToString();
+
+            TimeOfDay = day;
+            UpdateLighting(TimeOfDay / 24.0f);
+        }
+
+        private void UpdateLighting(float timePercent)
+        {
+            RenderSettings.ambientLight = LightingPreset.AmbientColor.Evaluate(timePercent);
+            RenderSettings.fogColor = LightingPreset.FogColor.Evaluate(timePercent);
+
+            directionalLight.color = LightingPreset.DirectionalColor.Evaluate(timePercent);
+            directionalLight.shadowStrength = LightingPreset.LightShadowStrength.Evaluate(timePercent).a;
+            directionalLight.transform.rotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+        }
+    }
+}
